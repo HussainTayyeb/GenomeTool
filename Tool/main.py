04ed39d05064b7ab822f72748b750af2b444c3b7
HussainTayyeb <h.tayyeb.r@gmail.com>
2021-01-16 18:44:34 +0100

    fetch taxid from names

--- Tool/main.py
+++ Tool/main.py
@@ -17,7 +17,7 @@
 #       - use TaxID from Nodes and apply on the accession ID Table ✔️
 
 #  ------ 4th Instance  -------
-#       - get tax_id from names table 
+#       - get tax_id from names table with name search
 #       - use the tax_id from names and search for parents_tax_id in nodes
 #       - retrieve all tax_id from parents_tax_id 
 #       - retrieve in Accession2TaxID all accession from the retrieved tax_id's
@@ -32,20 +32,14 @@ conn = sqlite3.connect('GCToolDB.db')  # You can create a new database by changi
 c = conn.cursor() # The database will be saved in the location where your 'py' file is saved
 
 
-
-#  ------  2nd Instance  -------
-#       - read from database ✔️
-#       - store into a variable ✔️
-#       - use a filter ✔️
-#       - use filter show taxID by filter ✔️
-
-
+"""
 hel = input("Filtern nach: ")
 b = "{}".format(hel)
 hel2 = input("Wert: ")
 b2 = "'{}'".format(hel2)
 
 showTaxID = c.execute("SELECT tax_id FROM Nodes WHERE {} = {}".format(b,b2)).fetchall()
+
 
 # use TaxID from Nodes and apply on the accession ID Table to get accession ✔️
 def useFilter(taxID):
@@ -57,3 +51,13 @@ def useFilter(taxID):
 for row in showTaxID:
     data = row[0]
     useFilter(data)
+"""""    
+
+name_input = input("Name: ")
+name_value = "{}".format(name_input)
+# ----- SET TO .fetchall()[:2] for testing purposes ------
+names_taxid = c.execute("SELECT name_tax_id FROM Names WHERE name_txt LIKE '%{}%'".format(name_value)).fetchall()[:2]
+
+for row in names_taxid:
+    data = row[0]
+    print(data)
