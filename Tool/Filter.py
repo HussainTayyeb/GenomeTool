3fd3c8ffa9c9562858c5a018cb1d1cab503e163a
HussainTayyeb <43951287+HussainTayyeb@users.noreply.github.com>
2021-03-31 19:52:17 +0200

    Create Filter.py

---
+++ Tool/Filter.py
@@ -0,0 +1,37 @@
+from Bio import SeqIO
+
+def fileParser(file):
+    seqObjlist = SeqIO.parse(f"{file}", "genbank")
+    return seqObjlist
+
+def writeToFasta(arr,outputFile):
+    with open(f"{outputFile}.fasta", "a") as output_handle:
+        SeqIO.write(arr, output_handle, "fasta")
+
+def endProduct(argFilter,resultSeqObj):
+    for func in argFilter:
+        #(func[0], filterValue)
+        resultSeqObj = func[0](resultSeqObj, func[1])
+    return resultSeqObj
+
+#Filters
+def filterMax(seqObj,filter):
+    filter = int(filter)
+    filtered_list = []
+    for record in seqObj:
+        if len(record.seq) < filter:
+            filtered_list.append(record) 
+    return filtered_list
+
+def filterMin(seqObj,filter):
+    filter = int(filter)
+    filtered_list = []
+    for record in seqObj:
+        if len(record.seq) > filter:
+            filtered_list.append(record)
+    return filtered_list
+
+filters = {
+    "filtermax": filterMax,
+    "filtermin": filterMin
+}
