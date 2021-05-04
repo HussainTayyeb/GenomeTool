a24c3858ed48d537e449b0678d20343f9bed14d1
HussainTayyeb <h.tayyeb.r@gmail.com>
2021-05-04 11:48:01 +0200

    deleted comment from filter.py

--- Tool/Filter.py
+++ Tool/Filter.py
@@ -20,14 +20,13 @@ filters = {
     "filtermin": filterMin
 }
 
-def useFilter(argFilter,filterSeqObj):
-    for func in argFilter:
-        filterSeqObj = func[0](filterSeqObj, func[1])
-    return filterSeqObj
-
-#function and parameter mapps the parsed arguments (--filter --parameter) for exportToFASTA
 def mapFilterParameter(filter_arg,parameter_arg):
     func_call = []
     for fil, par in zip(filter_arg, parameter_arg):
         func_call.append((filters[fil],(*par)))
     return func_call
+
+def useFilter(argFilter,filterSeqObj):
+    for func in argFilter:
+        filterSeqObj = func[0](filterSeqObj, func[1])
+    return filterSeqObj
